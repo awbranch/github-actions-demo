7eeaf1a9bac7a07dcb7e53568b1a8afdf8aade2f
Andrew Branch <branch@measuresforjustice.org>
2022-07-18 21:07:00 -0400

    Fixed linting bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Github Actions Demo
+          Github Actions Demo 2
         </h1>
 
         <Image src="/casper.jpg" alt="Casper" width="100%"/>
